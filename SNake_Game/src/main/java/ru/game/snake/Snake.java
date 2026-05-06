6bab790989b112dfb3f6a7c6a476f116375f4759
Allion591 <leo59158@yandex.ru>
2026-05-06 15:42:13 +0500

    Описал класс змейка, добавил список для части тел а так же, статус жизни, напрвление

--- SNake_Game/src/main/java/ru/game/snake/Snake.java
+++ SNake_Game/src/main/java/ru/game/snake/Snake.java
@@ -1,4 +1,25 @@
 package main.java.ru.game.snake;
 
+import java.util.List;
+
 public class Snake {
+    private List<SnakeSection> sections;
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
 }
