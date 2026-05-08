a325fa57a7fb1dd352aa1703040e93882b6e8b1a
Allion591 <leo59158@yandex.ru>
2026-05-08 10:52:49 +0500

    Реализовал класс Мышь, добавив конструктор и геттеры, так же добавил в класс комната методы съесть мышь и создать мышь

--- SNake_Game/src/main/java/ru/game/room/Room.java
+++ SNake_Game/src/main/java/ru/game/room/Room.java
@@ -57,4 +57,14 @@ public class Room {
 
     public void run() {}
     public void print() {}
+
+    public void createMouse() {
+        int x = (int) (Math.random() * width);
+        int y = (int) (Math.random() * height);
+        mouse = new Mouse(x, y);
+    }
+
+    public void eatMouse() {
+        createMouse();
+    }
 }
