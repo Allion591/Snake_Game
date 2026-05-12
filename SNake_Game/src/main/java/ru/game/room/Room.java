3809e3e2d0fca3e7d2806c86e546a2be0e2e1589
Allion591 <leo59158@yandex.ru>
2026-05-12 11:26:39 +0500

    Добавил методы проверки грниц и столкновения с телом змеи, так же направления движения змеи

--- SNake_Game/src/main/java/ru/game/room/Room.java
+++ SNake_Game/src/main/java/ru/game/room/Room.java
@@ -7,14 +7,13 @@ import main.java.ru.game.snake.SnakeDirection;
 import main.java.ru.game.snake.SnakeSection;
 
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class Room {
     private int width;
     private int height;
     private Snake snake;
     private Mouse mouse;
-
-    public static Room game;
 
     public Room(int width, int height, Snake snake) {
         this.width = width;
@@ -91,46 +90,61 @@ public class Room {
             sleep();        //пауза между ходами
         }
 
+        //Выводим сообщение "Game Over"
         System.out.println("Game Over!");
     }
 
+    /**
+     * Выводим на экран текущее состояние игры
+     */
     public void print() {
+        //Создаем массив, куда будем "рисовать" текущее состояние игры
         int[][] matrix = new int[height][width];
-        matrix[snake.getY()][snake.getX()] = 2;
 
-        for (int i = 1; i < snake.getSections().size(); i++) {
-            SnakeSection section = snake.getSections().get(i);
-            matrix[section.getY()][section.getX()] = 1;
+        //Рисуем все кусочки змеи
+        ArrayList<SnakeSection> sections = new ArrayList<SnakeSection>(snake.getSections());
+        for (SnakeSection snakeSection : sections) {
+            matrix[snakeSection.getY()][snakeSection.getX()] = 1;
         }
 
+        //Рисуем голову змеи (4 - если змея мертвая)
+        matrix[snake.getY()][snake.getX()] = snake.isAlive() ? 2 : 4;
+
+        //Рисуем мышь
         matrix[mouse.getY()][mouse.getX()] = 3;
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == 2) {
-                    System.out.print('X');
-                } else if (matrix[i][j] == 1) {
-                    System.out.print('x');
-                } else if (matrix[i][j] == 3) {
-                    System.out.print('^');
-                } else {
-                    System.out.print('.');
-                }
+        //Выводим все это на экран
+        String[] symbols = {" . ", " x ", " X ", "^_^", "RIP"};
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                System.out.print(symbols[matrix[y][x]]);
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
+    /**
+     * Метод вызывается, когда мышь съели
+     */
     public void eatMouse() {
         createMouse();
     }
 
+    /**
+     * Создает новую мышь
+     */
     public void createMouse() {
         int x = (int) (Math.random() * width);
         int y = (int) (Math.random() * height);
 
         mouse = new Mouse(x, y);
     }
+
+
+    public static Room game;
 
     public static void main(String[] args) {
         game = new Room(20, 20, new Snake(10, 10));
@@ -139,18 +153,19 @@ public class Room {
         game.run();
     }
 
+
+    private int initialDelay = 520;
+    private int delayStep = 20;
+
+    /**
+     * Программа делает паузу, длинна которой зависит от длинны змеи.
+     */
     public void sleep() {
         try {
             int level = snake.getSections().size();
-            if (level < 11) {
-                Thread.sleep(500);
-            } if (level == 11) {
-                Thread.sleep(300);
-            } if (level > 15) {
-                Thread.sleep(200);
-            }
+            int delay = level < 15 ? (initialDelay - delayStep * level) : 200;
+            Thread.sleep(delay);
         } catch (InterruptedException e) {
-            System.out.println("Ошибка " + e.getMessage());
         }
     }
 }
--- SNake_Game/src/main/java/ru/game/snake/Snake.java
+++ SNake_Game/src/main/java/ru/game/snake/Snake.java
@@ -1,5 +1,7 @@
 package main.java.ru.game.snake;
 
+import main.java.ru.game.room.Room;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +42,37 @@ public class Snake {
     }
 
     public void move() {
+        if (!isAlive()) {
+            return;
+        } if (direction == SnakeDirection.UP) {
+            move(0, -1);
+        } else if (direction == SnakeDirection.RIGHT) {
+            move(1, 0);
+        } else if (direction == SnakeDirection.DOWN) {
+            move(0, 1);
+        } else if (direction == SnakeDirection.LEFT) {
+            move(-1, 0);
+        }
+    }
 
+    public void move(int a, int b) {}
+
+    public void checkBorders(SnakeSection head) {
+        Room room = Room.game;
+        int height = room.getHeight();
+        int width = room.getWidth();
+
+        int y = head.getY();
+        int x = head.getX();
+
+        if (y < 0 || y >= height || x < 0 || x >= width) {
+            isAlive = false;
+        }
+    }
+
+    public void checkBody(SnakeSection head) {
+        if (sections.contains(head)) {
+            isAlive = false;
+        }
     }
 }
